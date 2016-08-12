4e7a96e8e3db45625f49966ef535ce668bc69a01
sas41 <berkonvo@gmail.com>
2016-08-13 00:43:12 +0300

    Changed Movement a bit

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -5,12 +5,17 @@ public class Move : MonoBehaviour {
 
     [Range(10, 100)]
     public float Speed;
-	// Use this for initialization
-	void Start() {
+    [Range(10, 100)]
+    public float JumpPower;
+    // Use this for initialization
+    void Start() {
 	}
 	
 	// Update is called once per frame
 	void Update() {
-        this.transform.Translate(Speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0, Space.World);	
-	}
+        float MovementSpeed = Speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        float JumpHeight = JumpPower * Input.GetAxis("Vertical") * Time.deltaTime;
+        this.transform.Translate(MovementSpeed, 0, 0, Space.World);
+        this.transform.Translate(0, JumpHeight, 0, Space.World);
+    }
 }
