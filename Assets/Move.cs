a51478543618643d90d1731d13d4246edbb2ba46
Borislav Kosharov <bosakmaw@gmail.com>
2016-08-12 23:54:11 +0300

    Circle :8ball: it moves

---
+++ Assets/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    [Range(10, 100)]
+    public float Speed;
+	// Use this for initialization
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        this.transform.Translate(Speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);	
+	}
+}
