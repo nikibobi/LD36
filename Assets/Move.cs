f995f0c736a65b07764e1368751c794cbf3518b0
Borislav Kosharov <bosakmaw@gmail.com>
2016-08-13 01:34:09 +0300

    :fast_forward:

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -15,8 +15,8 @@ public class Move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update() {
-        var movementSpeed = new Vector2(Speed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime, 0);
-        body.AddForce(movementSpeed, ForceMode2D.Impulse);
+        var movementSpeed = Speed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
+        body.AddForce(Vector2.right * movementSpeed, ForceMode2D.Impulse);
         if(Input.GetKeyDown(KeyCode.Space))
         {
             body.AddForce(Vector2.up * JumpPower, ForceMode2D.Force);
