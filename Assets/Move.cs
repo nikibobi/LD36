bfb9df20e8c9bccf5eaa556d5fa52c9ec25605ba
sas41 <berkonvo@gmail.com>
2016-08-13 05:26:46 +0300

    Quality Improvements
    Fixed Object Naming in the "Course" Level
    Fixed Unlimited Jumping
    Changed how Jumping works
    Improved movement system slightly
    Moved some code to their own functions
    Added grounded checks for the player

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -2,24 +2,62 @@
 
 public class Move : MonoBehaviour {
 
-    [Range(10, 100)]
-    public float Speed;
-    [Range(100, 1000)]
-    public float JumpPower;
+    [Range(1, 10)]
+    public float Speed, JumpPower , MaxVelocityX;
+    public float LineCastLength;
+    public LayerMask playerMask;
+
+    //public Transform RayCastStart, RayCastEnd;
 
     private Rigidbody2D body;
+
+
 	// Use this for initialization
 	void Start() {
         body = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update() {
-        var movementSpeed = Speed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime;
-        body.AddForce(Vector2.right * movementSpeed, ForceMode2D.Impulse);
-        if(Input.GetKeyDown(KeyCode.Space))
+
+    // Update is called once per frame
+    void Update() {
+
+        HandleMovement(Input.GetAxis("Horizontal"));
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            body.AddForce(Vector2.up * JumpPower, ForceMode2D.Force);
+            HandleJump();
+        }
+
+    }
+
+    public bool IsGrounded()
+    {
+        var linecastEnd = body.position + (Vector2.down * LineCastLength);
+        return Physics2D.Linecast(body.position, linecastEnd, playerMask);
+    }
+
+    public void HandleMovement(float direction)
+    {
+        body.AddForce(Vector2.right * direction, ForceMode2D.Impulse);
+
+        Vector2 currentVelocity = body.velocity;
+        if (currentVelocity.x > 0 && currentVelocity.x > MaxVelocityX)
+        {
+            currentVelocity.x = MaxVelocityX;
+        }
+        if (currentVelocity.x < 0 && currentVelocity.x < -MaxVelocityX)
+        {
+            currentVelocity.x = -MaxVelocityX;
+        }
+
+        body.velocity = currentVelocity;
+    }
+
+    public void HandleJump()
+    {
+        if (IsGrounded())
+        { 
+            body.velocity += JumpPower * Vector2.up;
         }
     }
+
 }
