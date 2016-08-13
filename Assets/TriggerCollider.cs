dbf8a3197ac859f84980bbc5fdbe200f98d2dcd3
DarkFluid <xepounn@abv.bg>
2016-08-14 00:38:27 +0300

    Added appearing text.
    You need to add the text PREFAB to the Man gameobject so that you can
    see what appears.
    Problem: The text appears before there is any collision has occured on
    the start of the level. Also i didn't figure out how to change the Y
    possition cuz of grill.

--- Assets/TriggerCollider.cs
+++ Assets/TriggerCollider.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class TriggerCollider : MonoBehaviour {
 
+    public Transform prefab;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("its happening");
+        if(!GameObject.Find(prefab.name))
+        Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        Destroy(GameObject.Find(prefab.name + "(Clone)"));
     }
 }
