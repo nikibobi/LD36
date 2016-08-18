ed4b98534edd4b490199614d2bf11f964700ea8c
sas41 <berkonvo@gmail.com>
2016-08-18 03:01:50 +0300

    Created a friendly following AI companion

--- Assets/Scripts/FriendlyNPC.cs
+++ Assets/Scripts/FriendlyNPC.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using UnityEngine;
+using System.Collections;
 
-public class Player : MonoBehaviour {
+public class FriendlyNPC : MonoBehaviour
+{
 
     [Range(0, 20)]
     public float Speed = 10;
@@ -12,33 +14,32 @@ public class Player : MonoBehaviour {
     public bool FloatyMovement = false;
     public float LineCastLength = 0.6f;
     public LayerMask PlayerMask;
+    public GameObject Target;
+    public float DetectionRange = 10;
+    public float MinimumRange = 2;
 
     private Rigidbody2D body;
-    private Action<float> move;
+    private Action<float> movement;
 
-	// Use this for initialization
-	void Start()
+
+    // Use this for initialization
+    void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        if(FloatyMovement)
+        if (FloatyMovement)
         {
-            move = FloatyMove;
+            movement = FloatyMove;
         }
         else
         {
-            move = SnappyMove;
+            movement = SnappyMove;
         }
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
-        move(Input.GetAxis("Horizontal"));
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            HandleJump();
-        }
+        MoveAI();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -59,11 +60,11 @@ public class Player : MonoBehaviour {
         return Physics2D.Linecast(body.position, body.position + (Vector2.down * LineCastLength), PlayerMask);
     }
 
-    private void HandleJump()
+    private void Jump()
     {
         if (IsGrounded())
         {
-            Debug.Log("Is grounded");
+            Debug.Log("NPC IS JUMPING");
             body.velocity += JumpPower * Vector2.up;
         }
     }
@@ -82,4 +83,32 @@ public class Player : MonoBehaviour {
         currentVel.x = direction * Speed;
         body.velocity = currentVel;
     }
+
+    private void MoveAI()
+    {
+        Vector2 TargetDirection = DetectTarget();
+
+        movement(TargetDirection.x);
+
+        if (TargetDirection.y > 0.1)
+        {
+            Jump();
+        }
+    }
+
+    private Vector2 DetectTarget()
+    {
+        float distance = Vector2.Distance(body.position, Target.transform.position);
+
+        if (distance < DetectionRange && distance > MinimumRange)
+        {
+            Vector2 destination = (Vector2)Target.transform.position - body.position;
+            float range = destination.magnitude;
+            Vector2 direction = destination / range;
+            return direction;
+        }
+
+        return new Vector2(0, 0);
+    }
+
 }
