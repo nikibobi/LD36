e7368fca760710c468027d665929334ca1d48e55
sas41 <berkonvo@gmail.com>
2016-08-26 07:24:50 +0300

    Trigger Collider

--- Assets/Scripts/TriggerCollider.cs
+++ Assets/Scripts/TriggerCollider.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TriggerCollider : MonoBehaviour {
 
-    public Transform prefab;
-    bool instatiatedText = false;
+    public GameObject prefab;
+    //bool spawned = false;
+    public Vector2 offset;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.name == "Player")
-        {
-            Vector3 possition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 2, gameObject.transform.position.z);
-            if (!GameObject.Find(prefab.name))
-                Instantiate(prefab, possition, gameObject.transform.rotation);
-            instatiatedText = true;
-        }
+            Instantiate(prefab, (Vector2)transform.position + offset, gameObject.transform.rotation);
     }
     void OnTriggerExit2D(Collider2D collider)
     {
-        if(instatiatedText) Destroy(GameObject.Find(prefab.name + ("(Clone)")));
+        var obj = GameObject.Find(prefab.name + "(Clone)");
+        if (obj != null)
+            Destroy(obj);
     }
+
+
 }
