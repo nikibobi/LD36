c013afe3e54a1ed05b27667a719d03f64e9e8613
XepouH <xepounn@abv.bg>
2016-08-27 15:15:45 +0300

    Health script.

---
+++ Assets/Scripts/EnemyNPC.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyNPC : MonoBehaviour {
+
+    public GameObject Target;
+    public float DetectionRange = 10;
+    public float MinimumRange = 2;
+    public float JumpTriggerHeight = 2;
+    public float attackDamage = 0;
+    public float attackSpeed = 0f;
+    private float lastAttackTime = 0;
+
+    private Movement movement;
+    private Rigidbody2D body;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        movement = GetComponent<Movement>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 destination = (Vector2)Target.transform.position - body.position;
+        float distance = destination.magnitude;
+        Vector2 direction = destination / distance;
+        float heightDifference = Target.transform.position.y - body.position.y;
+
+        if (distance < DetectionRange)
+        {
+            if (Mathf.Round(distance) > MinimumRange)
+            {
+                movement.Move(direction.x);
+            }
+            else
+            {
+                movement.Move((direction.x / MinimumRange) * (Mathf.Round(distance) - 1));
+                //Attack timing.
+                if (Time.time > (lastAttackTime + attackSpeed) && !Target.GetComponent<Player>().dead)
+                { 
+                    lastAttackTime = Time.time;
+                    print(this.name + " attacked!");
+                }
+            }
+
+            if (heightDifference > JumpTriggerHeight)
+            {
+                movement.Jump();
+            }
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,7 @@ using Spine.Unity;
 
 public class Player : MonoBehaviour
 {
-    [Range(0, 100)]
-    public int hp;
-    [Range(0, 100)]
-    public int armor;
-    bool dead = false;
+    public bool dead = false;   
 
     private Movement movement;
     private SkeletonAnimation spine;
@@ -22,50 +18,47 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (hp <= 0 && !dead)
+        if (!dead)
         {
-            Destroy(gameObject.GetComponent<Player>());
-            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;    
+
+            movement = null;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
             dead = true;
             //string animation = Random.Range(0, 2) == 0 ? "DeathBackward" : "DeathForward";
             spine.state.SetAnimation(0, "DeathBackward", false);
             return;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            spine.skeleton.FlipX = false;
-            spine.AnimationName = "Move";
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            spine.skeleton.FlipX = true;
-            spine.AnimationName = "Move";
-        }
-        else
-        {
-            spine.AnimationName = "Idle";
-        }
-        movement.Move(Input.GetAxis("Horizontal"));
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            spine.state.SetAnimation(1, "Attack", false);
-            movement.Jump();
-        }
+            if (Input.GetKey(KeyCode.D))
+            {
+                spine.skeleton.FlipX = false;
+                spine.AnimationName = "Move";
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                spine.skeleton.FlipX = true;
+                spine.AnimationName = "Move";
+            }
+            else
+            {
+                spine.AnimationName = "Idle";
+            }
+            movement.Move(Input.GetAxis("Horizontal"));
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            spine.state.SetAnimation(0, "Shoot", false);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                spine.state.SetAnimation(1, "Attack", false);
+                movement.Jump();
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                spine.state.SetAnimation(0, "Shoot", false);
+            }
         }
     }
 
-    public void LoseHp(int damageTaken)
+    void deadAnimation(bool death)
     {
-        //League's formula for damage reduction.
-        hp -= Mathf.Abs(100 / (100 + armor) - damageTaken);
-    }
-    public void GainHp(int healAmount)
-    {
-        hp += healAmount;
+
     }
 }
