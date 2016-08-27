ed070b084f6003107ec0eb213c48b5fee6d8cc54
sas41 <berkonvo@gmail.com>
2016-08-27 07:28:08 +0300

    Added a Bow and Arrow

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour {
+
+    private Rigidbody2D body;
+    private bool collided = false;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!collided)
+        {
+            Vector2 velocity = body.velocity;
+            float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+            this.gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (true)//collision.transform.tag == "Ground")
+        {
+            collided = true;
+            //gameObject.transform.parent = collision.transform;
+            body.isKinematic = true;
+            //Destroy(body);
+            //Destroy(GetComponent<CircleCollider2D>());
+            //body.freezeRotation = true;
+            //body.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+    }
+}
---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bow : MonoBehaviour {
+
+    private Vector2 ClickPos, CurrnetPos;
+    private bool clicked = false;
+    private float power;
+    private Vector2 direction;
+
+    public GameObject ArrowType;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickPos = Input.mousePosition;
+            clicked = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            clicked = false;
+            if (power > 1)
+            {
+                GameObject arrow = (GameObject)Instantiate(ArrowType, (Vector2)transform.position + (direction * 1), transform.rotation);
+                Rigidbody2D arrowBody = arrow.GetComponent<Rigidbody2D>();
+                arrowBody.velocity = direction * power;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            CurrnetPos = Input.mousePosition;
+
+            Vector2 difference = ClickPos - CurrnetPos;
+            float distance = difference.magnitude;
+            direction = difference / distance;
+            power = distance/10;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            this.gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        }
+
+    }
+}
