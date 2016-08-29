ef81c3fddba78b297bd7d2df5e83e4959f3ca884
sas41 <berkonvo@gmail.com>
2016-08-29 18:54:38 +0300

    Polishing Update

---
+++ Assets/Scripts/ParalaxBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParalaxBackground : MonoBehaviour {
+
+    public Transform CenterObject;
+
+    [Range(1, 2)]
+    public float ParallaxRate;
+
+    private Vector2 centerObjectStart;
+    private Vector2 imageStart;
+
+    void Start()
+    {
+        centerObjectStart = CenterObject.position;
+        imageStart = this.gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        Vector2 offset = (centerObjectStart - (Vector2)CenterObject.position);
+        this.gameObject.transform.position = imageStart - (offset / ParallaxRate);
+    }
+}
